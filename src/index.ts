1e70da3b06fc007f7513e8bd9d6d1b69496bc93e
Liam McLoughlin <lmcloughlin@fitbit.com>
2018-10-30 10:08:12 +0000

    Fix accidentally flipped FW errata verison check
    Signed-off-by: Liam McLoughlin <lmcloughlin@fitbit.com>

--- src/index.ts
+++ src/index.ts
@@ -172,7 +172,7 @@ export function buildDeviceComponents({
     })!,
   ];
 
-  if (sdkVersion().major >= 3) {
+  if (sdkVersion().major < 3) {
     deviceJSPipeline.push(
       gulpMagicString(errataPrimaryExpressionInSwitch),
     );
