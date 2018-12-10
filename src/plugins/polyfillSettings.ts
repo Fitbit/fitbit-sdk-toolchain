b5cbeb12723a726d35dd3cda932b1ce4a4d1d5ea
Liam McLoughlin <hexxeh@hexxeh.net>
2018-12-10 19:13:07 +0000

    Be more restrictive about where PO files must be located (#29)
    Signed-off-by: Liam McLoughlin <lmcloughlin@fitbit.com>

--- src/plugins/polyfillSettings.ts
+++ src/plugins/polyfillSettings.ts
@@ -2,5 +2,5 @@ import i18nPolyfill from './i18nPolyfill';
 import polyfill from './polyfill';
 
 export default function polyfillCompanion() {
-  return polyfill(i18nPolyfill('settings/**/*.po'));
+  return polyfill(i18nPolyfill('settings/i18n/*.po'));
 }
