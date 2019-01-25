389fb4dcc50387b20b9bbb18e3098a97976b2d81
Liam McLoughlin <lmcloughlin@fitbit.com>
2019-01-25 12:07:26 +0000

    Configure terser to emit code compatible with iOS 10
    Signed-off-by: Liam McLoughlin <lmcloughlin@fitbit.com>

--- src/types/terser/index.d.ts
+++ src/types/terser/index.d.ts
@@ -11,10 +11,12 @@ interface OutputOptions {
 }
 
 export interface TerserOptions {
+  // tslint:disable-next-line max-union-size
   ecma?: 5 | 6 | 7 | 8;
   compress?: CompressOptions | false;
   mangle?: MangleOptions | false;
   output?: OutputOptions;
+  safari10?: boolean;
 }
 
 interface TerserResult {}
