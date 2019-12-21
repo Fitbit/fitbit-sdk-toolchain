0e2b6173d95cd30dc8da8c00b4d5c233cd2e884d
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2019-12-21 13:33:24 +0100

    Update settings scaffolding
    Inlined component to infer types
    Add children content to Section
    
    Signed-off-by: Sergio Morchón Poveda <sergio.morchon@outlook.com>

--- scaffold/settings/index.jsx
+++ scaffold/settings/index.jsx
@@ -1,15 +1,16 @@
-function settingsComponent(props) {
-  return (
-    <Page>
-      <Section
-        title={
-          <Text bold align="center">
-            App Settings
-          </Text>
-        }
-      />
-    </Page>
-  );
-}
-
-registerSettingsPage(settingsComponent);
+registerSettingsPage(({ settings }) => (
+  <Page>
+    <Section
+      title={
+        <Text bold align="center">
+          App Settings
+        </Text>
+      }
+    >
+      <Text>
+        This is a very basic demo settings page to show off some of the current
+        capabilities of the Companion Settings library.
+      </Text>
+    </Section>
+  </Page>
+));
