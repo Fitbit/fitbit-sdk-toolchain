e3e2d6959b02428bc21d14e3bec6623711471455
Liam McLoughlin <lmcloughlin@fitbit.com>
2020-01-24 17:23:36 +0000

    Add support for new languages
    Signed-off-by: Liam McLoughlin <lmcloughlin@fitbit.com>

--- src/languageTag.ts
+++ src/languageTag.ts
@@ -1,17 +1,5 @@
-export const supportedTags = [
-  'de-DE',
-  'en-US',
-  'es-ES',
-  'fr-FR',
-  'it-IT',
-  'ja-JP',
-  'ko-KR',
-  'nl-NL',
-  'sv-SE',
-  'zh-CN',
-  'zh-TW',
-];
+import { Locales } from './ProjectConfiguration';
 
 export function validateLanguageTag(tagString: string) {
-  return supportedTags.includes(tagString);
+  return Object.keys(Locales).includes(tagString);
 }
