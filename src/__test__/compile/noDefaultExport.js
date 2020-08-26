90b9cd7607f64d23e1f7f33d2602482da63df35f
Claudiu Nedelcu <cnedelcu@fitbit.com>
2020-08-26 17:15:20 +0300

    Use module namespace as default export for built-in modules without default exports
    Signed-off-by: Claudiu Nedelcu <cnedelcu@fitbit.com>

---
+++ src/__test__/compile/noDefaultExport.js
@@ -0,0 +1,21 @@
+import crypto from 'crypto';
+import document from 'document';
+import fs from 'fs';
+import jpeg from 'jpeg';
+import power from 'power';
+import scientific from 'scientific';
+import signal from 'scientific/signal';
+import system from 'system';
+import userActivity from 'user-activity';
+import userSettings from 'user-settings';
+
+console.log('typeof crypto: ' + typeof crypto);
+console.log('typeof document: ' + typeof document);
+console.log('typeof fs: ' + typeof fs);
+console.log('typeof jpeg: ' + typeof jpeg);
+console.log('typeof power: ' + typeof power);
+console.log('typeof scientific: ' + typeof scientific);
+console.log('typeof signal: ' + typeof signal);
+console.log('typeof system: ' + typeof system);
+console.log('typeof userActivity: ' + typeof userActivity);
+console.log('typeof userSettings: ' + typeof userSettings);
